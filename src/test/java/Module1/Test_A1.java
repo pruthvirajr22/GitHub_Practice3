b7b2c5aad43d6c093faec6cdeef223f690c88d7c
91968 <91968@pruthvi007>
2023-05-14 19:14:44 +0530

    method m4 added to the test 'Test_A1'

--- src/test/java/Module1/Test_A1.java
+++ src/test/java/Module1/Test_A1.java
@@ -3,7 +3,6 @@ package Module1;
 public class Test_A1
 {
  public void m1() 
- 
  {
 	 System.out.println("this is m1 from Test_A1");
  }
@@ -12,4 +11,8 @@ public void m3()
  {
 	 System.out.println("this is m3 from Test_A1");
  }
+public void m4() 
+{
+	 System.out.println("this is m3 from Test_A1");
+}
 }
