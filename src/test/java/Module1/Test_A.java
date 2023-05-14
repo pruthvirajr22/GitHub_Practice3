a1c26fb5b87659cfbae8b4e465aef784ce392c0e
91968 <91968@pruthvi007>
2023-05-14 15:19:08 +0530

    created new test 'Test_A' having methods m1 and m2

---
+++ src/test/java/Module1/Test_A.java
@@ -0,0 +1,13 @@
+package Module1;
+
+public class Test_A
+{
+ public void m1() 
+ {
+	 System.out.println("m1 from Test_A");
+ }
+ public void m2() 
+ {
+	 System.out.println("m2 from Test_A");
+ }
+}
